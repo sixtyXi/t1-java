262091e4ccc2683ae7e78677b81a360a4c7cea09
Sergei Karaulov <sixty.xi@gmail.com>
2025-05-17 15:01:47 +0400

    task-6: refactor list response

--- src/main/java/t1/task6/controller/ProductController.java
+++ src/main/java/t1/task6/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import t1.task6.dto.ListResponse;
 import t1.task6.projection.ProductProjection;
 import t1.task6.service.ProductService;
 
@@ -23,7 +24,8 @@ public class ProductController {
     }
 
     @GetMapping(value = "/user/{userId}")
-    public List<ProductProjection> getProductsByUserId(@PathVariable("userId") Long userId) {
-        return productService.findProductsByUserId(userId);
+    public ListResponse<ProductProjection> getProductsByUserId(@PathVariable("userId") Long userId) {
+        List<ProductProjection> products = productService.findProductsByUserId(userId);
+        return new ListResponse<>(products);
     }
 }
